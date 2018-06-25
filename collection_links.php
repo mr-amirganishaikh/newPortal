1b99f5cb7d7ab9367187aa0ac8347c0f3a30b002
AmirAlgorythma <amir@algorythma.com>
2018-06-25 14:42:26 +0400

    sample design page created

---
+++ collection_links.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <title>New Portal</title>
+    <?php include '_partial/head.inc.php' ?>
+</head>
+
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>URL</th>
+                <th>Concept</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>
+                    <a href="https://facebook.design/">Design Link</a>
+                </th>
+                <td>
+                    Home page design to display top news - 1
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <a href="https://mdbootstrap.com/previews/free-templates/three-columns-listing/">Design Link</a>
+                </th>
+                <td>
+                    Home page design to display top news - 2
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
