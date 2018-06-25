1f5356647f8911506e5fafeadb3a3d3225cdee2d
AmirAlgorythma <amir@algorythma.com>
2018-06-25 12:33:50 +0400

    base code setup

---
+++ _partial/head.inc.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width:device-width, initial-scale=1, shrink-to-fit=no" />
+<meta http-equiv="x-ua-compatible" content="ie=edge" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+    crossorigin="anonymous" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,500|Raleway|Roboto" rel="stylesheet" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+    crossorigin="anonymous" />
+<!--[if lt IE 9]>
+    <script src=https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js></script>
+    <script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script>
+<![endif]-->
+<link href="../assets/css/styles.css" rel="stylesheet" />
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>New Portal</title>
+    <?php include '_partial/head.inc.php' ?>
+</head>
+
+<body>
+    <header>
+        <?php include '_partial/header.inc.php' ?>
+    </header>
+    <div class="clearfix"></div>
+
+    <main></main>
+    <div class="clearfix"></div>
+
+    <footer>
+        <?php include '_partial/footer.inc.php' ?>
+    </footer>
+    <div class="clearfix"></div>
+
+    <?php include '_partial/scripts.inc.php' ?>
+</body>
+
+</html>
